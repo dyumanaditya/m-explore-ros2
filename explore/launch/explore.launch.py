4a36b7002350ca0ffe106640e8cea27481f0e4c9
Dyuman Aditya <dyuman2001@gmail.com>
2025-05-05 08:54:40 +0000

    param changes

--- explore/launch/explore.launch.py
+++ explore/launch/explore.launch.py
@@ -11,7 +11,7 @@ from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
     ld = LaunchDescription()
     config = os.path.join(
-        get_package_share_directory("explore_lite"), "config", "params.yaml"
+        get_package_share_directory("explore_lite"), "config", "params_costmap.yaml"
     )
     use_sim_time = LaunchConfiguration("use_sim_time")
     namespace = LaunchConfiguration("namespace")
